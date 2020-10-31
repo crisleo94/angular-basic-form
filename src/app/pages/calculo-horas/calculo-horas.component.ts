d1ae122db6816f78cc829c37748a2a5467277f9d
crisleo94 <cristhian.reinoso@live.com>
2020-10-30 19:53:54 -0500

    add servicio and calculo component

---
+++ src/app/pages/calculo-horas/calculo-horas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculo-horas',
+  templateUrl: './calculo-horas.component.html',
+  styleUrls: ['./calculo-horas.component.scss']
+})
+export class CalculoHorasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/servicio-tecnico/servicio-tecnico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servicio-tecnico',
+  templateUrl: './servicio-tecnico.component.html',
+  styleUrls: ['./servicio-tecnico.component.scss']
+})
+export class ServicioTecnicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
